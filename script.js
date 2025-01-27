abfd4a117ddb79844750ef477661f7302b5ba090
pflashgary <pflashgary@gmail.com>
2025-01-27 18:43:00 +0100

    start from scartch

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get the element that will be typed
+const typed = new Typed('.typed', {
+    strings: ['an intelligent failure', 'amusingly thought-provoking', 'an acquired taste'],
+    typeSpeed: 75,
+    backSpeed: 75,
+    loop: true
+});
